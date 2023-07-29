07b63c1d8ac0aab843f91702b25f20b4710fa02e
Joan Abad <j.abad.reynoso@gmail.com>
2023-07-29 14:32:49 +0200

    Exampled code using VAO and VBO new classes.

--- SandboxApp.cpp
+++ SandboxApp.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include "Rendering/RenderingSystem.h"
 #include "Window/Public/WindowManager.h"
+#include "Window/Public/Viewport.h"
+#include "Rendering/Buffers/VBO.h"
+#include "Rendering/Buffers/VAO.h"
 
 SandboxApp::SandboxApp() : YorkiEngineApp()
 {
@@ -13,34 +16,7 @@ SandboxApp::SandboxApp() : YorkiEngineApp()
 
 void SandboxApp::OnCreateApplicationCallback()
 {
- /*
-    Shader squareShader("res/shaders/Square.shader");
-    Shader secondSquareShader("res/shaders/SecondSquare.shader");
     Shader triangleShader("res/shaders/Triangle.shader");
-
-    // Vertices of the left square
-    std::vector<float> leftSquareVertices = {
-        -0.75f, -0.5f, 0.0f, // Bottom-left vertex
-        -0.25f, -0.5f, 0.0f, // Bottom-right vertex
-        -0.75f, 0.5f, 0.0f,  // Top-left vertex
-        -0.25f, 0.5f, 0.0f    // Top-right vertex
-    };
-
-    // Vertices of the right square
-    std::vector<float> rightSquareVertices = {
-        0.25f, -0.5f, 0.0f,  // Bottom-left vertex
-        0.75f, -0.5f, 0.0f,  // Bottom-right vertex
-        0.25f, 0.5f, 0.0f,   // Top-left vertex
-        0.75f, 0.5f, 0.0f    // Top-right vertex
-    };
-
-    // Indices of both squares to be drawn with GL_TRIANGLES
-    std::vector<unsigned int> indices = {
-        0, 1, 2, // First triangle (left square)
-        1, 3, 2, // Second triangle (left square)
-        4, 5, 6, // First triangle (right square)
-        5, 7, 6  // Second triangle (right square)
-    };
 
     std::vector<float>vertices = {
         // positions       
@@ -49,13 +25,20 @@ void SandboxApp::OnCreateApplicationCallback()
          0.0f,  0.5f, 0.0f,// top 
     };
 
-    //RenderObject* firstSquare = new RenderObject(leftSquareVertices, indices, squareShader);
-    //RenderObject* secondSquare = new RenderObject(rightSquareVertices, indices, secondSquareShader);
-    RenderObject* triangle = new RenderObject(vertices, triangleShader);
+    RenderObject* triangle = new RenderObject;
+    triangle->AttachShader(triangleShader);
+    
+    VBO* vbo = new VBO(vertices, 3, triangle->GetVAO());
+    triangle->AddVBOBuffer(*vbo);
+    
+
+    Window* window = &WindowManager::GetActiveWindow();
+    Viewport* viewport = dynamic_cast<Viewport*>(window);
+    viewport->AddRenderObject(*triangle);
+    int i = 0; 
     //triangle->buffer.AddBufferData(vertices);
 
     //triangle->AttachShader(triangleShader);
-        */
 }
 
 void SandboxApp::OnPreDraw()
